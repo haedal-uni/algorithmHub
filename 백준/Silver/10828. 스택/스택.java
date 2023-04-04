94b770f147e45c1c84e736224e79e8cee4ad901c
soyeon <luora@naver.com>
2023-04-04 23:46:34 +0900

    [Silver IV] Title: 스택, Time: 188 ms, Memory: 18864 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,67 +1,48 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] stack;
-    public static int sp = 0;
-
     public static void main(String[] args) throws IOException {
+        Stack<Integer> s = new Stack<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int num = Integer.parseInt(br.readLine());
-        stack = new int[num];
         StringTokenizer st;
         for(int i=0; i<num; i++){
             st = new StringTokenizer(br.readLine(), " ");
             switch (st.nextToken()){
                 case "push":
-                    push(Integer.parseInt(st.nextToken()));
+                    s.push(Integer.parseInt(st.nextToken()));
                     break;
                 case "pop":
-                    sb.append(pop()).append('\n');
+                    if(s.empty()){
+                        sb.append("-1" + "\n");
+                    }else{
+                        sb.append(s.pop()).append("\n");
+                    }
                     break;
                 case "size":
-                    sb.append(size()).append('\n');
+                    sb.append(s.size()).append("\n");
                     break;
                 case "empty":
-                    sb.append(empty()).append('\n');
+                    if(s.empty()){
+                        sb.append("1" + "\n");
+                    } else {
+                        sb.append("0" + "\n");
+                    }
                     break;
                 case "top" :
-                    sb.append(top()).append('\n');
+                    if(s.empty()){
+                        sb.append("-1" + "\n");
+                    }else{
+                        sb.append(s.peek()).append("\n");
+                    }
                     break;
             }
         }
         System.out.println(sb);
-    }
-
-    public static void push(int x){
-        stack[sp] = x;
-        sp++;
-
-    }
-    public static int pop(){
-        if(sp==0){
-            return -1;
-        }
-        int res = stack[sp-1];
-        stack[sp-1] = 0;
-        sp--;
-        return res;
-    }
-    public static int size(){
-        return sp;
-    }
-    public static int empty(){
-        if (sp==0){
-            return 1;
-        }
-        return 0;
-    }
-
-    public static int top(){
-        if (sp==0){
-            return -1;
-        }
-        return stack[sp-1];
     }
 }
