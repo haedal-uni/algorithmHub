98a5b93b9f37c8a7d11361f72bc056c4bd0fec88
soyeon <luora@naver.com>
2023-04-12 01:41:12 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 880 ms, Memory: 89244 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,55 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int[] visited;
-    static List<List<Integer>> graph = new ArrayList<>();
+    static List<PriorityQueue<Integer>> graph = new ArrayList<>();
     static int count;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
 
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
 
-        visited = new int[V+1];
-        for (int i = 0; i < V+1; i++) {
-            graph.add(new ArrayList<>());
+        visited = new int[V + 1];
+        for (int i = 0; i < V + 1; i++) {
+            graph.add(new PriorityQueue<>());
         }
-
-        for (int i=0; i<E; i++){
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int V1 = Integer.parseInt(st.nextToken());
             int E1 = Integer.parseInt(st.nextToken());
             graph.get(V1).add(E1);
             graph.get(E1).add(V1);
         }
-
-        for(int i = 1; i < graph.size(); i++) {
-            Collections.sort(graph.get(i));
-        }
-
         count = 1;
         dfs(R);
-        for(int i = 1; i < visited.length; i++) {
+        for (int i = 1; i < visited.length; i++) {
             sb.append(visited[i]).append("\n");
         }
         System.out.println(sb);
     }
-    public static void dfs(int V){
+
+    public static void dfs(int V) {
         visited[V] = count;
-        for(int i=0; i<graph.get(V).size(); i++){
-            int n = graph.get(V).get(i);
-            if(visited[n] == 0){
+        while (!graph.get(V).isEmpty()) {
+            int N = graph.get(V).poll();
+            if (visited[N] == 0) {
                 count++;
-                dfs(n);
+                dfs(N);
             }
         }
     }
