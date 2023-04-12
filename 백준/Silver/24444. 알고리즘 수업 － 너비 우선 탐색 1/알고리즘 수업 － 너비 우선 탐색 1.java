d727d71efd085e65d869d84a9169d5f055e9edbb
soyeon <luora@naver.com>
2023-04-13 01:07:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 940 ms, Memory: 96060 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -6,7 +6,8 @@ import java.util.*;
 public class Main {
     static int[] visited;
     static int count;
-    static List<List<Integer>> graph = new ArrayList<>();
+    static List<PriorityQueue<Integer>> graph = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -16,12 +17,12 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
 
-        visited = new int[N+1];
-        for(int i=0; i<N+1; i++){
-            graph.add(new ArrayList<>());
+        visited = new int[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new PriorityQueue<>());
         }
 
-        for(int i=0; i<M; i++){
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int N1 = Integer.parseInt(st.nextToken());
             int M1 = Integer.parseInt(st.nextToken());
@@ -29,32 +30,27 @@ public class Main {
             graph.get(N1).add(M1);
         }
 
-        for (int i=1; i<graph.size(); i++) {
-            Collections.sort(graph.get(i));
-        }
-
         count = 1;
         bfs(R);
-        for(int i=1; i<visited.length; i++){
+        for (int i = 1; i < visited.length; i++) {
             sb.append(visited[i]).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    public static void bfs(int n){
+    public static void bfs(int n) {
         Deque<Integer> queue = new LinkedList<>();
         queue.offer(n);
-        visited[n] = count;
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int x = queue.poll();
-            for(int i=0; i<graph.get(x).size(); i++){
-                int node = graph.get(x).get(i);
-                if(visited[node] == 0){
-                    visited[node]=++count;
-                    queue.offer(node);
+            if (visited[x] == 0) {
+                visited[x] = count++;
+                while (!graph.get(x).isEmpty()) {
+                    queue.offer(graph.get(x).poll());
                 }
             }
         }
+
     }
 }
