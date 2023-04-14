a808152abe487081c3f8a5ed8c83ccd291a7c804
soyeon <luora@naver.com>
2023-04-14 22:51:48 +0900

    [Silver III] Title: N과 M (1), Time: 268 ms, Memory: 22880 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -15,19 +15,19 @@ public class Main {
         sb = new StringBuilder();
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N+1];
+        arr = new int[M];
         count = new boolean[N+1];
         num(0);
         System.out.println(sb);
     }
-    public static void num(int index){
+    public static void num(int index){// M1 n 3
         if(index == M){
             for(int i=0; i<M; i++){
                 sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
+            return;
         }
-
         for(int i=1; i<N+1; i++){
             if (!count[i]) {
                 arr[index] = i;
