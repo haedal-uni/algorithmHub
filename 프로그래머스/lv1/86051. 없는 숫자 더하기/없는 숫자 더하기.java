2f8d417f94772c6901a5ace9018feee47ba2dfd4
soyeon <luora@naver.com>
2023-04-23 02:37:41 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int total = 1+2+3+4+5+6+7+8+9;
+        for(int i=0; i<numbers.length; i++){
+            total -= numbers[i];
+        }
+        return total;
+    }
+}
