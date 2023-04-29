c44943a1f57f014e140eae64b262963d7f73981e
soyeon <luora@naver.com>
2023-04-30 01:46:20 +0900

    [level 1] Title: 직사각형 별찍기, Time: 179.06 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -1,11 +1,9 @@
-import java.lang.StringBuilder;
 import java.util.StringTokenizer;
 import java.io.*;
 class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         String answer = "";
