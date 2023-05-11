d92638cebeaf0a0fa15945c89e342384884223a3
soyeon <luora@naver.com>
2023-05-12 01:50:47 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.48 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int[] answer = new int[num_list.length - 5];
+        int num = 0;
+        for(int i=5; i<num_list.length; i++){
+            answer[num] = num_list[i];
+            num++;
+        }
+        return answer;
+    }
+}
