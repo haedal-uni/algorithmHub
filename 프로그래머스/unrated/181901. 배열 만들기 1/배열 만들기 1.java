cc6a52ee3686d5d007c58d4e30b075b8372e4c44
soyeon <luora@naver.com>
2023-05-15 20:00:31 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
+        int num = 0;
+        for(int i=k; i<=n; i+=k){
+            answer[num] = i;
+            num++;
+        }
+        return answer;
+    }
+}
