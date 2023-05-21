eb4e2ace48b0078f5dfd1c51310d2cc3ef77c989
soyeon <luora@naver.com>
2023-05-21 17:34:33 +0900

    [unrated] Title: 문자열 붙여서 출력하기, Time: 145.38 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb= new StringBuilder();
+        sb.append(st.nextToken()).append(st.nextToken());
+        System.out.println(sb.toString());
+    }
+}
