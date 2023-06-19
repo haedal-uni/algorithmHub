e35ba34957d1ecee033896f120cd6fb4d535e171
soyeon <luora@naver.com>
2023-06-20 01:53:39 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
-        if(my_string.length() >= is_prefix.length() && my_string.substring(0, is_prefix.length()).equals(is_prefix)){
+        if(my_string.startsWith(is_prefix)){
             answer = 1;
         }
         return answer;
