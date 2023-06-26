ae268233b7b0f45823b34c635acde1761bde6222
soyeon <luora@naver.com>
2023-06-27 02:08:41 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,12 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        int i=0;
-        while(true){
-            if(!n_str.split("")[i].equals("0")){
-                break;
-            }
-            i++;
-        }
-        return n_str.substring(i);
+        return String.valueOf(Integer.parseInt(n_str));
     }
 }
