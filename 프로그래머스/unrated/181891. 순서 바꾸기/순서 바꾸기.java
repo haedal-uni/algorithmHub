3612ac2efcf258c16ae3c8863d416514a6813def
soyeon <luora@naver.com>
2023-07-10 03:58:08 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,19 +1,13 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[num_list.length];
-        int num = 0;
-        int len = num_list.length;
-        if(n==len){
-            return num_list;
-        }else{
-            for(int i=n; i<len; i++){
-                answer[num] = num_list[i];
-                num++;
-                if(num_list.length-1==i){
-                    i = -1;
-                    len = n;
-                }
-            } 
+        int num = n;
+        for(int i=0; i<num_list.length; i++){
+            if(num==num_list.length){
+                num=0;
+            }
+            answer[i] = num_list[num];
+            num++;
         }
         return answer;
     }
