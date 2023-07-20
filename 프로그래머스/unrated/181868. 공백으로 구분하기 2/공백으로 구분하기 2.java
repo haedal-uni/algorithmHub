d5018cc053bb41787ccb1d4949a9f5b36de36d7f
soyeon <luora@naver.com>
2023-07-21 01:14:38 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.06 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -2,22 +2,9 @@ import java.util.*;
 class Solution {
     public String[] solution(String my_string) {
         StringTokenizer st = new StringTokenizer(my_string, " ");
-        int num = 0;
-        boolean status = false;
         String[] answer = new String[st.countTokens()];
-        for(int i=0; i<my_string.length(); i++){
-            if(!my_string.split("")[i].equals(" ")){
-                status = true;
-            }else{ 
-                if(status){
-                    answer[num] = st.nextToken();
-                    num++;
-                    status = false;
-                }
-           }   
-        }
-        if((num+1)==answer.length){
-            answer[num] = st.nextToken();
+        for(int i=0; i<answer.length; i++){
+            answer[i] = st.nextToken();
         }
         return answer;
     }
