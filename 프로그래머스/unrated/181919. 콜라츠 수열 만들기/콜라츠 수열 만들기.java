5168a6bb116f3980458c4dced7416ed19fd6838a
soyeon <luora@naver.com>
2023-07-23 20:46:08 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.06 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -3,17 +3,13 @@ class Solution {
     public int[] solution(int n) {
         List<Integer> list = new ArrayList<>();
         list.add(n);
-        while (true){
-            if(n==1){
-                break;
-            }
+        while (n>1){
             if(n%2==0){
                 n = n/2;
-                list.add(n);
             }else{
                 n = 3*n+1;
-                list.add(n);
             }
+            list.add(n);
         }
         int[] answer = new int[list.size()];
         for(int i=0; i<list.size(); i++){
