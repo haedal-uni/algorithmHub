b2b4f6cc265ead113de72aaf275d1fffb9fcf301
soyeon <luora@naver.com>
2023-08-04 00:20:20 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,7 +1,10 @@
+import java.lang.*;
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder sb = new StringBuilder();
         String num = my_string.substring(0, s);
         String num1 = my_string.substring(s+overwrite_string.length());
-        return num + overwrite_string + num1;
+        sb.append(num).append(overwrite_string).append(num1);
+        return sb.toString();
     }
 }
