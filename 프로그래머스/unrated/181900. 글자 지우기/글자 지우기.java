091f33407b29658fa6a4d126e6d4c96d516a3a6e
soyeon <luora@naver.com>
2023-09-19 23:22:11 +0900

    [unrated] Title: 글자 지우기, Time: 0.27 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -1,19 +1,12 @@
-import java.util.*;
 class Solution {
     public String solution(String my_string, int[] indices) {
         StringBuilder sb = new StringBuilder();
         String[] ans = my_string.split("");
-        int cnt = 0;
-        Arrays.sort(indices);
+        for(int i=0; i<indices.length; i++){
+            ans[indices[i]] = "";
+        }
         for(int i=0; i<ans.length; i++){
-            if(i == indices[cnt]){
-                cnt++;
-                if(cnt==indices.length){
-                    cnt = 0;
-                }
-            }else{
-                sb.append(ans[i]);
-            }
+            sb.append(ans[i]);
         }
         return sb.toString();
     }
