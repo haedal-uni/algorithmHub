d1e5eece69e3912bd5a8183ad9e70b6cf394c46d
soyeon <luora@naver.com>
2023-09-30 00:18:34 +0900

    [level 0] Title: A로 B 만들기, Time: 0.25 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,19 +1,10 @@
 class Solution {
     public int solution(String before, String after) {
         int answer = 0;
-        StringBuilder sb = new StringBuilder();
-        sb.append(before);
-        int count = 0;
-        int sum = 0;
         for(int i=0; i<before.length(); i++){
-            count+= (int) before.charAt(i);
+            after = after.replaceFirst(before.split("")[i], "");
         }
-        for(int i=0; i<after.length(); i++){
-            sum += (int) after.charAt(i);
-        }
-        if(after.equals(sb.reverse().toString())){
-            answer = 1;
-        } else if(count==sum){
+        if(after.length()==0){
             answer = 1;
         }
         return answer;
