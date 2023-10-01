0e7cbf1741eecaf7c4e60ff3e3bf7c29208b0347
soyeon <luora@naver.com>
2023-10-01 19:49:56 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 2*k-1;
-        while(true){
-            if(answer<=numbers.length){
-                break;
-            }
-            answer -= numbers.length;
-        }
-        return answer;
+        return (2*(k-1) % numbers.length) + 1;
     }
 }
