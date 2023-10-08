9238936caaec3b07514478ce21743e1983aa7762
soyeon <luora@naver.com>
2023-10-08 22:04:51 +0900

    [level 0] Title: 문자열 밀기, Time: 1.58 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,20 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        int count = 0;
-        if(!A.equals(B)){
-           while(true){
-            if(A.equals(B)){
-                break;
-            }else if(count==A.length()){
-                answer = -1;
-                break;
-            }
-               A = A.substring(A.length()-1) + A.substring(0,A.length()-1);
-               count++;
-               answer++;
-           } 
-        }
-        return answer;
+        return (B+B).indexOf(A);
     }
 }
