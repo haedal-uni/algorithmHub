43ca8315cf5d51702d953c1a2530d0e774ed616a
soyeon <luora@naver.com>
2023-10-22 01:52:55 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 1.19 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[arr.length];
         int num1 = 0;
         int num2 = 0;
         for(int i=0; i<queries.length; i++){
             num1 = queries[i][0];
             num2 = queries[i][1];
             for(int j=num1; j<=num2; j++){
-                answer[j]++;
+                arr[j]++;
             }
         }
-        for(int i=0; i<arr.length; i++){
-            answer[i] = answer[i]+arr[i];
-        }
-        return answer;
+        return arr;
     }
 }
