84e441ad57b884e9ab35ef0efaaf57ba153ce52f
soyeon <luora@naver.com>
2023-12-14 02:43:38 +0900

    [level unrated] Title: 5명씩, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.py
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.py
@@ -1,7 +1,2 @@
 def solution(names):
-    answer = []
-    for i in range(len(names)) :
-        if (5*i) >= len(names) :
-            break
-        answer.append(names[5*i])
-    return answer
+    return names[::5]
