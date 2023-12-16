62940b2dd44290d748f8fb1c139da0a0099be659
soyeon <luora@naver.com>
2023-12-16 12:32:24 +0900

    [level unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,10 +1,6 @@
 import java.lang.*;
 class Solution {
     public String solution(String my_string, int k) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<k; i++){
-            sb.append(my_string);
-        }
-        return sb.toString();
+        return my_string.repeat(k);
     }
 }
