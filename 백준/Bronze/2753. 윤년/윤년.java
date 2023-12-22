f0c5a2f730450fe9b7414bd740d23f60fab9d474
soyeon <luora@naver.com>
2023-12-22 19:56:59 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,12 +1,13 @@
 import java.io.*;
+
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int year = Integer.parseInt(br.readLine());
+        int answer = 0;
         if(year%4==0 && year%100!=0 || year%400==0){
-            System.out.print(1);
-        }else{
-            System.out.print(0);
+            answer = 1;
         }
+        System.out.println(answer);
     }
 }
