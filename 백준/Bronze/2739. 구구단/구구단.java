51ceb2145b91d2d9030b3eb98c7c4a213d7b5430
soyeon <luora@naver.com>
2023-12-23 23:17:32 +0900

    [Bronze V] Title: 구구단, Time: 152 ms, Memory: 16152 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -4,8 +4,14 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int i=1; i<10; i++){
-            System.out.println(num + " * " + i + " = " + num*i);
+            if(i==9){
+                sb.append(num + " * " + i + " = " + num*i);
+            }else{
+                sb.append(num + " * " + i + " = " + num*i).append("\n");
+            }
         }
+        System.out.print(sb);
     }
 }
