4e6ee2ee00c12c54372fb5c15afb3dbcd4ee9fb1
soyeon <luora@naver.com>
2024-03-03 17:13:25 +0900

    [Bronze II] Title: 소수 찾기, Time: 128 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        int count = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        while(st.hasMoreTokens()) {
+            boolean x = true;
+            int num = Integer.parseInt(st.nextToken());
+            if(num == 1) {
+                continue;
+            }
+            for(int i=2; i<=Math.sqrt(num); i++) {
+                if(num%i==0) {
+                    x = false;
+                    break;
+                }
+            }
+            if(x){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
