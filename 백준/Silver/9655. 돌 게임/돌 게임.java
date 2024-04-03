4738271bb7d802b2d28e95ab907ef55d03a82bc8
soyeon <luora@naver.com>
2024-04-03 15:31:32 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int count = 1;
+        String[] ans = {"SK","CY"};
+        while (x!=0){
+            if(x-3>0){
+                x-=3;
+            }else{
+                x-= 1;
+            }
+            if(count==0){
+                count=1;
+            }else {
+                count=0;
+            }
+        }
+        System.out.println(ans[count]);
+    }
+}
