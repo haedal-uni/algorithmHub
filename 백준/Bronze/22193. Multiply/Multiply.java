e4c29d3f7088aee05817545fd1f45d7593a8b3ea
soyeon <luora@naver.com>
2024-04-10 14:17:20 +0900

    [Bronze V] Title: Multiply, Time: 652 ms, Memory: 55984 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,10 +1,11 @@
 import java.io.*;
+import java.math.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
-        long x = Long.parseLong(br.readLine());
-        long y = Long.parseLong(br.readLine());
-        System.out.println(x*y);
+        BigInteger x = new BigInteger(br.readLine());
+        BigInteger y = new BigInteger(br.readLine());
+        System.out.println(x.multiply(y));
     }
 }
