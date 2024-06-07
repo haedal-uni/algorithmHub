66102e7e69413e24010315c5b06c0a31cb5b61a9
soyeon <luora@naver.com>
2024-06-07 13:41:19 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 216 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine()," ");
-        String[] a = st.nextToken().split("");
-        String[] b = st.nextToken().split("");
+        String a = st.nextToken();
+        String b = st.nextToken();
         long sum = 0;
-        for (String s : a) {
-            for (String str : b) {
-                sum += Long.parseLong(s) * Long.parseLong(str);
+        for (int i=0; i<a.length(); i++) {
+            for (int j=0; j<b.length(); j++) {
+                sum += (long) (a.charAt(i) - '0') *(b.charAt(j)-'0');
             }
         }
         System.out.println(sum);
