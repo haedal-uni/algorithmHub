6663dba31604b4186d1931efecab187e9ace1f6c
soyeon <luora@naver.com>
2024-06-14 17:24:36 +0900

    [Bronze III] Title: 앵그리 창영, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/3034. 앵그리 창영/앵그리 창영.java
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.java
@@ -11,7 +11,7 @@ public class Main {
         int x = 0;
         for(int i=0; i<n; i++){
             x = Integer.parseInt(br.readLine());
-            if(Math.pow(w,2)+Math.pow(h,2)>=x*x){
+            if(Math.pow(w,2)+Math.pow(h,2)>=Math.pow(x,2)){
                 sb.append("DA").append("\n");
             } else{
                 sb.append("NE").append("\n");
