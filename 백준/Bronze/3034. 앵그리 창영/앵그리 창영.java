8409dee433d2a35b6d91cb7a1d2c808096688830
soyeon <luora@naver.com>
2024-06-14 17:22:01 +0900

    [Bronze III] Title: 앵그리 창영, Time: 128 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int x = 0;
+        for(int i=0; i<n; i++){
+            x = Integer.parseInt(br.readLine());
+            if((w*w)+(h*h)>=x*x){
+                sb.append("DA").append("\n");
+            } else{
+                sb.append("NE").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
