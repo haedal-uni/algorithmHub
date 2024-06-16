1407ba3e01275510836679dbaee3129470488564
soyeon <luora@naver.com>
2024-06-16 17:01:26 +0900

    [Bronze V] Title: 초코바, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        if(n*100>=m){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
