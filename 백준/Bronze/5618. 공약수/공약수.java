b780103d2884fb01d9154ea20b3b465891270dae
soyeon <luora@naver.com>
2024-06-30 17:12:58 +0900

    [Bronze II] Title: 공약수, Time: 432 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.java
+++ 백준/Bronze/5618. 공약수/공약수.java
@@ -3,29 +3,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[Integer.parseInt(br.readLine())];
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         int x = 0;
         int y = 0;
         int z = 0;
-        for(int i=0; i<arr.length; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-        x = arr[0];
-        y = arr[1];
-        if(arr.length==3){
-            z = arr[2];
-            for(int i=1; i<=x; i++){
-                if(x%i==0&&y%i==0&&z%i==0){
+        if(n==2){
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            for(int i=1; i<=Math.min(x,y); i++){
+                if(x%i==0&&y%i==0){
                     sb.append(i).append("\n");
                 }
             }
         }else{
-            for(int i=1; i<=x; i++){
-                if(x%i==0&&y%i==0){
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            z = Integer.parseInt(st.nextToken());
+            for(int i=1; i<=Math.min(x,Math.min(y,z)); i++){
+                if(x%i==0&&y%i==0&&z%i==0){
                     sb.append(i).append("\n");
                 }
             }
