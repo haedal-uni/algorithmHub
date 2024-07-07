846918e86daaef4b88249879d24dddeb09e97a6e
soyeon <luora@naver.com>
2024-07-07 14:19:53 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 120 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine())+1;
-        System.out.println(n*2 + " "+n*3);
+        StringBuilder sb = new StringBuilder();
+        sb.append(n*2).append(" ").append(n*3);
+        System.out.println(sb);
     }
 }
