ea9df31cc457b1b11666d07cb05b1207b95c099d
soyeon <luora@naver.com>
2024-08-22 17:27:49 +0900

    [Bronze IV] Title: Betting, Time: 128 ms, Memory: 16344 KB -BaekjoonHub

---
+++ 백준/Bronze/24751. Betting/Betting.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        System.out.println(100.0 / x + "\n" + 100.0 / (100 - x));
+    }
+}
