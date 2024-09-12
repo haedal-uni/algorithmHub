c83d1f1bd55a117171a12502406408554fe9c626
soyeon <luora@naver.com>
2024-09-12 14:59:17 +0900

    [Bronze V] Title: Hook, Time: 88 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#  # #### #### #  #").append("\n");
+        sb.append("#### #  # #  # # #").append("\n");
+        sb.append("#### #  # #  # # #").append("\n");
+        sb.append("#  # #### #### #  #");
+        System.out.println(sb);
+    }
+}
