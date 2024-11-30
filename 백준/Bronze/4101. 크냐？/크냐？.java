646d3698c9238f73ad9c8f8b28a79d444fc60cbe
soyeon <luora@naver.com>
2024-11-30 16:52:35 +0900

    [Bronze V] Title: 크냐?, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static Deque<Integer> deque = new ArrayDeque<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int a = 0;
+        int b = 0;
+        while (true){
+            st = new StringTokenizer(br.readLine());
+            a=Integer.parseInt(st.nextToken());
+            b=Integer.parseInt(st.nextToken());
+            if(a==0 && b==0){
+                break;
+            }
+            if(a>b){
+                sb.append("Yes").append("\n");
+            }else{
+                sb.append("No").append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
