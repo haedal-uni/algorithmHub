0452a59ab7bc5c0f6eb7ba03727c7dcd100c97de
soyeon <luora@naver.com>
2025-01-13 16:37:37 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.java
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
