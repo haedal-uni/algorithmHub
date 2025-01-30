eb0ce11ce8cfb18e2446911632ea0384490cf2f4
soyeon <luora@naver.com>
2025-01-30 14:36:40 +0900

    [Bronze IV] Title: Which Alien?, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        if(a>2 && b<5){
+            sb.append("TroyMartian").append("\n");
+        }
+        if(a<7 && b>1){
+            sb.append("VladSaturnian").append("\n");
+        }
+        if(a<3 && b<4){
+            sb.append("GraemeMercurian").append("\n");
+        }
+        System.out.print(sb);
+    }
+}
