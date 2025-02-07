3e52392dcea3dfd2fa439d1112ce031594c4e38d
soyeon <luora@naver.com>
2025-02-07 22:41:50 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,3 +1,3 @@
-select COUNT(*) COUNT
+select COUNT(GENOTYPE) COUNT
 from ECOLI_DATA
-where (GENOTYPE & 2) = 0 &&  (GENOTYPE & 1 > 0 || GENOTYPE & 4 > 0); 
+where (GENOTYPE & 2) = 0 &&  (GENOTYPE & 1 > 0 || GENOTYPE & 4 > 0);
