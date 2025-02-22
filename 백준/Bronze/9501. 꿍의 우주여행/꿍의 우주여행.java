245cd95eeca7716fb5ad78f1d43538cd131d8fc2
soyeon <luora@naver.com>
2025-02-22 21:06:13 +0900

    [Bronze III] Title: 꿍의 우주여행, Time: 128 ms, Memory: 15432 KB -BaekjoonHub

--- 백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.java
+++ 백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.java
@@ -5,17 +5,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
+        int n = 0;
+        int d = 0;
+        int v = 0;
+        float f = 0.0f;
+        float c = 0.0f;
         int count = 0;
         for(int i=0; i<t; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            d = Integer.parseInt(st.nextToken());
             count = 0;
             for(int j=0; j<n; j++){
                 st = new StringTokenizer(br.readLine());
-                int v = Integer.parseInt(st.nextToken());
-                float f = Integer.parseInt(st.nextToken());
-                float c = Integer.parseInt(st.nextToken());
+                v = Integer.parseInt(st.nextToken());
+                f = Integer.parseInt(st.nextToken());
+                c = Integer.parseInt(st.nextToken());
                 if(d-v*(f/c)<=0){
                     count++;
                 }
