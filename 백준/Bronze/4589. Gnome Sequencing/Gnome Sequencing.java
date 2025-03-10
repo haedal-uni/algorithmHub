59a9a2aa572844dc08bbed54e921410c3f2c6d99
soyeon <luora@naver.com>
2025-03-10 15:16:56 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 100 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.java
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.java
@@ -7,13 +7,17 @@ public class Main {
         sb.append("Gnomes:").append("\n");
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[3];
+        int x = 0;
+        int y = 0;
         for(int i=0; i<n; i++){
-            String[] s = br.readLine().split(" ");
-            arr[0] = Integer.parseInt(s[0]);
-            arr[1] = Integer.parseInt(s[1]);
-            arr[2] = Integer.parseInt(s[2]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            arr[0] = x;
+            arr[1] = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            arr[2] = y;
             Arrays.sort(arr);
-            if(s[0].equals(arr[0]+"")&&s[2].equals(arr[2]+"")||s[0].equals(arr[2]+"")&&s[2].equals(arr[0]+"")){
+            if(x==arr[0] && y==arr[2]|| x==arr[2]&&y==arr[0]){
                 sb.append("Ordered").append("\n");
             }else{
                 sb.append("Unordered").append("\n");
