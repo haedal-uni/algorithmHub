cd418555aeb16eef64b450d895ec9d5ecd35ee44
soyeon <luora@naver.com>
2025-03-16 15:23:50 +0900

    [Bronze II] Title: 2009년, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/2948. 2009년/2009년.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.time.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int d = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        LocalDate data = LocalDate.of(2009,m,d);
+        switch (data.getDayOfWeek().getValue()){
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thursday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            case 7:
+                System.out.print("Sunday");
+                break;
+        }
+    }
+}
