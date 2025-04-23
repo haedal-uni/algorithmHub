5c5f707c320366fa5ae0d485db7bd83541fdd1c7
soyeon <luora@naver.com>
2025-04-23 23:49:35 +0900

    [Bronze IV] Title: SMS from MCHS, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int t1 = Integer.parseInt(st.nextToken());
+        int v1 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int t2 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
+        if(t2<0 && v2>=10){
+            sb.append("A storm warning for tomorrow! Be careful and stay home if possible!").append("\n");
+        }else if(t1>t2){
+            sb.append("MCHS warns! Low temperature is expected tomorrow.").append("\n");
+        }else if(v2>v1){
+            sb.append("MCHS warns! Strong wind is expected tomorrow.").append("\n");
+        }else{
+            sb.append("No message").append("\n");
+        }
+        System.out.print(sb);
+    }
+}
