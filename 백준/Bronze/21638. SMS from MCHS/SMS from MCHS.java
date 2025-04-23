d61f7f6ebdee6f3ca89cddd2006b459fa95298c7
soyeon <luora@naver.com>
2025-04-23 23:50:09 +0900

    [Bronze IV] Title: SMS from MCHS, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.java
+++ 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.java
@@ -11,11 +11,11 @@ public class Main {
         int t2 = Integer.parseInt(st.nextToken());
         int v2 = Integer.parseInt(st.nextToken());
         if(t2<0 && v2>=10){
-            sb.append("A storm warning for tomorrow! Be careful and stay home if possible!").append("\n");
+            sb.append("A storm warning for tomorrow! Be careful and stay home if possible!");
         }else if(t1>t2){
-            sb.append("MCHS warns! Low temperature is expected tomorrow.").append("\n");
+            sb.append("MCHS warns! Low temperature is expected tomorrow.");
         }else if(v2>v1){
-            sb.append("MCHS warns! Strong wind is expected tomorrow.").append("\n");
+            sb.append("MCHS warns! Strong wind is expected tomorrow.");
         }else{
             sb.append("No message").append("\n");
         }
