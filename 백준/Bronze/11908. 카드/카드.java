2009a8fd36f030c888f715db06121b0616dee6e7
soyeon <luora@naver.com>
2025-05-16 14:59:29 +0900

    [Bronze III] Title: 카드, Time: 112 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Bronze/11908. 카드/카드.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for(int i=0; i<n-1; i++) {
+            sum+=arr[i];
+        }
+        System.out.print(sum);
+    }
+}
