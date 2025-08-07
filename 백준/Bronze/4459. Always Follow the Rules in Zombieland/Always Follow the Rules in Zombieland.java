a9b5b945ead8686ea9124ae041abc20228d9e661
soyeon <luora@naver.com>
2025-08-07 21:23:23 +0900

    [Bronze III] Title: Always Follow the Rules in Zombieland, Time: 96 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/4459. Always Follow the Rules in Zombieland/Always Follow the Rules in Zombieland.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        String[] s = new String[n];
+        for(int i=0; i<n; i++) {
+            s[i] = br.readLine();
+        }
+        n = Integer.parseInt(br.readLine());
+        int x = 0;
+        for(int i=0; i<n; i++) {
+            x = Integer.parseInt(br.readLine());
+            if(x>s.length || x<1) {
+                sb.append("Rule ").append(x).append(": No such rule").append("\n");
+            }else{
+                sb.append("Rule ").append(x).append(": ").append(s[x-1]).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
