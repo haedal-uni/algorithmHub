2f5957db0854949739c429134dbeffcefd8b7a4e
soyeon <luora@naver.com>
2025-08-15 11:48:23 +0900

    [Bronze III] Title: Overdraft, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/24609. Overdraft/Overdraft.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int x = 0;
+        for(int i=0; i<n; i++){
+            sum+=Integer.parseInt(br.readLine());
+            if(sum<0){
+                x-=sum;
+                sum=0;
+            }
+        }
+        System.out.print(x);
+    }
+}
