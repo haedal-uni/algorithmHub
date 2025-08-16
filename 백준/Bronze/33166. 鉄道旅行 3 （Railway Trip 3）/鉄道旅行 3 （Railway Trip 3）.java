50e168546e658d9fbd8d1608ead8aebb77994c54
soyeon <luora@naver.com>
2025-08-16 13:40:28 +0900

    [Bronze IV] Title: 鉄道旅行 3 (Railway Trip 3), Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/33166. 鉄道旅行 3 （Railway Trip 3）/鉄道旅行 3 （Railway Trip 3）.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if(p>q){
+            System.out.print(q*a);
+        }else{
+            System.out.print(p*a + (q-p)*b);
+        }
+    }
+}
