bb626739e6f0196fba72e82222c231f64d589e0c
soyeon <luora@naver.com>
2025-08-27 21:23:21 +0900

    [Bronze I] Title: A/B - 2, Time: 156 ms, Memory: 16764 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.java
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -1,11 +1,22 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        double a = Integer.parseInt(st.nextToken());
-        double b = Integer.parseInt(st.nextToken());
-        System.out.printf("%.1000f", a/b);
+        BigDecimal a = new BigDecimal(st.nextToken());
+        BigDecimal b = new BigDecimal(st.nextToken());
+        MathContext mc = new MathContext(2100);
+        BigDecimal x = a.divide(b, mc);
+        String s = x.toPlainString();
+        if (s.contains(".")) {
+            int idx = s.indexOf(".");
+            int decimals = s.length()-idx-1;
+            if (decimals > 2000) {
+                s = s.substring(0, idx + 2001);
+            }
+        }
+        System.out.print(s);
     }
 }
