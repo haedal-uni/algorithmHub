dd2f9cf3095f71d42712a414d5c288e06970fda0
soyeon <luora@naver.com>
2025-10-25 11:54:41 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 108 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            String s = br.readLine();
+            if(s.contains("S")){
+                System.out.print(s);
+                break;
+            }
+        }
+    }
+}
