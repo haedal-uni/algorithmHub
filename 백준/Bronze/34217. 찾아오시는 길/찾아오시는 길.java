eb8b13a98408e8ed023b01d672bdb5480c25de9b
soyeon <luora@naver.com>
2025-11-13 20:26:32 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        a+=Integer.parseInt(st.nextToken());
+        b+=Integer.parseInt(st.nextToken());
+        if(a>b){
+            System.out.print("Yongdap");
+        }else if(a==b){
+            System.out.print("Either");
+        }else{
+            System.out.print("Hanyang Univ.");
+        }
+    }
+}
