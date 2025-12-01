2d14517cf2076802dfd7130c0f34e03e9dc28ba1
soyeon <luora@naver.com>
2025-12-01 20:31:08 +0900

    [Bronze III] Title: gahui and sousenkyo 2, Time: 192 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Bronze/30792. gahui and sousenkyo 2/gahui and sousenkyo 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int x = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        arr[0] = x;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=1; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for(int i=0; i<n; i++){
+            if(arr[i] == x){
+                System.out.print(n-i);
+            }
+        }
+    }
+}
